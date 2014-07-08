429632e68cb583d5e24d6f2ad9b07974f94ff4dc
Colin Hill <colin.james.hill@gmail.com>
2014-07-08 12:28:06 -0700

    Made Zeroth common assets as project

--- deploy.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-from subprocess import call
-
-call("python ../hect/Tools/create_zip.py Deploy/Hect.data ../hect/Assets/Hect Hect", shell=True)
-call("python ../hect/Tools/create_zip.py Deploy/Zeroth.data Assets", shell=True)
