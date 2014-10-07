0e20dfb74be5acfc568367f18b771a5b1f758007
Colin Hill <colin.james.hill@gmail.com>
2014-10-06 21:24:38 -0700

    Formatting JSON files

--- Tools/format.py
+++ Tools/format.py
@@ -1,5 +1,5 @@
-from subprocess import call
+import subprocess
 
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\Source\\*.cpp ..\\Source\\*.h ..\\Source\\.inl", shell=True)
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Engine\\*.cpp ..\\..\\hect\\Engine\\*.h ..\\..\\hect\\Engine\\.inl", shell=True)
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Tests\\*.cpp ..\\..\\hect\\Tests\\*.h ..\\..\\hect\\Tests\\.inl", shell=True)
+subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\Source\\*.cpp ..\\Source\\*.h ..\\Source\\.inl", shell=True)
+subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Engine\\*.cpp ..\\..\\hect\\Engine\\*.h ..\\..\\hect\\Engine\\.inl", shell=True)
+subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Tests\\*.cpp ..\\..\\hect\\Tests\\*.h ..\\..\\hect\\Tests\\.inl", shell=True)
