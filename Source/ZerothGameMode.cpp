e66cc11ec16fa8180cd1d0076034a5db90bf804b
Colin Hill <colin.james.hill@gmail.com>
2014-10-07 21:09:22 -0700

    Supporting changes

--- Source/ZerothGameMode.cpp
+++ Source/ZerothGameMode.cpp
@@ -6,6 +6,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "ZerothGameMode.h"
 
+#include <Hect/IO/Data.h>
 #include <Hect/IO/JsonDecoder.h>
 #include <Hect/Logic/Systems/InputSystem.h>
 #include <Hect/Runtime/Engine.h>
@@ -13,31 +14,24 @@
 using namespace zeroth;
 
 ZerothGameMode::ZerothGameMode(Engine& engine) :
-    GameMode(engine, TimeSpan::fromSeconds((Real)1 / (Real)60))
+    GameMode(engine, TimeSpan::fromSeconds((Real)1 / (Real)60)),
+    _world(engine)
 {
-    engine.renderSystem().addWorld(_world);
+    RenderSystem& renderSystem = engine.renderSystem();
+    renderSystem.addWorld(_world);
 
-    InputSystem& inputSystem = _world.system<InputSystem>();
-    for (const JsonValue& axisValue : engine.config()["inputAxes"])
-    {
-        InputAxis axis;
+    AssetCache& assetCache = engine.assetCache();
 
-        JsonDecoder decoder(axisValue);
-        decoder >> decodeValue(axis);
-
-        inputSystem.addAxis(axis);
-    }
-
-    AssetCache::SelectDirectoryScope scope(engine.assetCache(), "Test");
-
-    AssetHandle<JsonValue> worldJsonValue = engine.assetCache().getHandle<JsonValue>("World.world");
-    JsonDecoder decoder(*worldJsonValue, engine.assetCache());
-    decoder >> beginObject() >> decodeValue(_world) >> endObject();
+    AssetCache::SelectDirectoryScope scope(assetCache, "Test");
+    AssetHandle<JsonValue> worldJsonValue = assetCache.getHandle<JsonValue>("World.world");
+    JsonDecoder decoder(*worldJsonValue, assetCache);
+    decoder >> decodeValue(_world);
 }
 
 ZerothGameMode::~ZerothGameMode()
 {
-    engine().renderSystem().removeWorld(_world);
+    RenderSystem& renderSystem = engine().renderSystem();
+    renderSystem.removeWorld(_world);
 }
 
 void ZerothGameMode::tick()
