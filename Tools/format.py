54614db0edf1c44089c298169d36933a6baa92cd
Colin Hill <colin.james.hill@gmail.com>
2014-10-25 17:02:55 -0700

    Migrating to CMake

--- Tools/format.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\Source\\*.cpp ..\\Source\\*.h ..\\Source\\.inl", shell=True)
-subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Engine\\*.cpp ..\\..\\hect\\Engine\\*.h ..\\..\\hect\\Engine\\.inl", shell=True)
-subprocess.call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\hect\\Tests\\*.cpp ..\\..\\hect\\Tests\\*.h ..\\..\\hect\\Tests\\.inl", shell=True)
