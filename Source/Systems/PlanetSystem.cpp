9e205b2c2d2f9ea8c640c1c1bddd93f26215e6ae
Colin Hill <colin.james.hill@gmail.com>
2015-12-20 17:21:16 -0800

    ...

--- Source/Systems/PlanetSystem.cpp
+++ Source/Systems/PlanetSystem.cpp
@@ -10,6 +10,7 @@ using namespace zeroth;
 
 PlanetSystem::PlanetSystem(Engine& engine, Scene& scene) :
     System(engine, scene),
+    _assetCache(engine.assetCache()),
     _boundingBoxSystem(scene.system<BoundingBoxSystem>()),
     _cameraSystem(scene.system<CameraSystem>()),
     _transformSystem(scene.system<TransformSystem>())
@@ -31,85 +32,142 @@ void PlanetSystem::tick(double timeStep)
         {
             Vector3 cameraPosition = activeCamera->position;
 
-            // Adapt planetoids
+            // Adapt planets
         }
     }
 }
 
-void PlanetSystem::onComponentAdded(Planet::Iterator planetoid)
+void PlanetSystem::onComponentAdded(Planet::Iterator planet)
 {
-    Entity::Iterator entity = planetoid->entity();
-
-    if (entity->hasComponent<Transform>())
+    if (_planet)
     {
-        throw InvalidOperation("Planet cannot be created with a transform; they can only be centered at the origin");
+        throw InvalidOperation("Only one planet can exist in a scene");
     }
-
-    Transform::Iterator transform = entity->addComponent<Transform>();
-    transform->mobility = Mobility::Static;
-
-    BoundingBox::Iterator boundingBox = entity->component<BoundingBox>();
-    if (!boundingBox)
+    else
     {
-        boundingBox = entity->addComponent<BoundingBox>();
-        boundingBox->adaptive = true;
-    }
+        _planet = planet;
 
-    createRootPatch(planetoid, CubeSide::PositiveX);
-    createRootPatch(planetoid, CubeSide::NegativeX);
-    createRootPatch(planetoid, CubeSide::PositiveY);
-    createRootPatch(planetoid, CubeSide::NegativeY);
-    createRootPatch(planetoid, CubeSide::PositiveZ);
-    createRootPatch(planetoid, CubeSide::NegativeZ);
+        Entity::Iterator entity = planet->entity();
 
-    if (_boundingBoxSystem)
-    {
-        _boundingBoxSystem->update(*boundingBox);
+        // Add the transform component
+        Transform::Iterator transform = entity->component<Transform>();
+        if (!transform)
+        {
+            transform = entity->addComponent<Transform>();
+        }
+        transform->mobility = Mobility::Static;
+        transform->localPosition = Vector3::Zero;
+        transform->localScale = Vector3::One;
+        transform->localRotation = Quaternion::Identity;
+
+        // Add the bounding box component
+        BoundingBox::Iterator boundingBox = entity->component<BoundingBox>();
+        if (!boundingBox)
+        {
+            boundingBox = entity->addComponent<BoundingBox>();
+            boundingBox->adaptive = true;
+        }
+
+        // Create the root patches
+        Entity::Iterator positiveX = createRootPatch(planet, CubeSide::PositiveX);
+        createRootPatch(planet, CubeSide::NegativeX);
+        createRootPatch(planet, CubeSide::PositiveY);
+        createRootPatch(planet, CubeSide::NegativeY);
+        createRootPatch(planet, CubeSide::PositiveZ);
+        createRootPatch(planet, CubeSide::NegativeZ);
+
+        split(positiveX->component<PlanetPatch>());
+
+        // Update the bounding box
+        if (_boundingBoxSystem)
+        {
+            _boundingBoxSystem->update(*boundingBox);
+        }
     }
 }
 
-Entity::Iterator PlanetSystem::createRootPatch(Planet::Iterator planetoid, CubeSide cubeSide)
+void PlanetSystem::split(PlanetPatch::Iterator patch)
+{
+    const Vector3& up = cubeSideUpVector(patch->cubeSide);
+    const Vector3& right = cubeSideRightVector(patch->cubeSide);
+    const Vector3 front = up.cross(right);
+
+    Entity::Iterator entity = patch->entity();
+
+    Model::Iterator model = entity->component<Model>();
+    if (model)
+    {
+        model->visible = false;
+    }
+
+    double quarterSize = patch->halfSize * 0.5;
+    for (int y = -1; y <= 1; y += 2)
+    {
+        for (int x = -1; x <= 1; x += 2)
+        {
+            createPatch(entity, patch->cubeSide, right * quarterSize * x + front * quarterSize * y);
+        }
+    }
+}
+
+Entity::Iterator PlanetSystem::createPatch(Entity::Iterator parent, CubeSide cubeSide, const Vector3& relativePosition)
 {
     const Vector3& up = cubeSideUpVector(cubeSide);
     const Vector3& right = cubeSideRightVector(cubeSide);
 
+    Planet::Iterator planet = parent->component<Planet>();
+    PlanetPatch::Iterator parentPatch = parent->component<PlanetPatch>();
+
     Entity::Iterator patchEntity = scene().createEntity();
     patchEntity->setTransient(true);
-
-    Vector3 patchPosition = up * planetoid->meanRadius;
-
+    
     Transform::Iterator transform = patchEntity->addComponent<Transform>();
     transform->mobility = Mobility::Dynamic;
-    transform->localPosition = patchPosition;
+    transform->localPosition = relativePosition;
 
     BoundingBox::Iterator boundingBox = patchEntity->addComponent<BoundingBox>();
 
     PlanetPatch::Iterator patch = patchEntity->addComponent<PlanetPatch>();
     patch->cubeSide = cubeSide;
-    patch->halfSize = planetoid->meanRadius;
 
-    Mesh::Handle mesh = buildPatchMesh(planetoid, patch, patchPosition);
+    if (planet)
+    {
+        patch->halfSize = planet->meanRadius;
+    }
+    else
+    {
+        patch->halfSize = parentPatch->halfSize * 0.5;
+    }
+
+    Mesh::Handle mesh = buildPatchMesh(_planet, patch, relativePosition);
 
     Model::Iterator model = patchEntity->addComponent<Model>();
-    model->addSurface(mesh, planetoid->patchMaterial);
+    model->addSurface(mesh, _planet->patchMaterial);
+
+    Mesh::Handle cubeMesh = _assetCache.getHandle<Mesh>("Test/Cube.Main.mesh");
+    model->addSurface(cubeMesh, _planet->patchMaterial);
 
     patchEntity->activate();
-
-    planetoid->entity()->addChild(*patchEntity);
+    parent->addChild(*patchEntity);
 
     return patchEntity;
 }
 
-Mesh::Handle PlanetSystem::buildPatchMesh(Planet::Iterator planetoid, PlanetPatch::Iterator patch, const Vector3& relativePosition)
+Entity::Iterator PlanetSystem::createRootPatch(Planet::Iterator planet, CubeSide cubeSide)
+{
+    return createPatch(planet->entity(), cubeSide, cubeSideUpVector(cubeSide) * planet->meanRadius);
+}
+
+Mesh::Handle PlanetSystem::buildPatchMesh(Planet::Iterator planet, PlanetPatch::Iterator patch, const Vector3& relativePosition)
 {
     const Vector3& up = cubeSideUpVector(patch->cubeSide);
     const Vector3& right = cubeSideRightVector(patch->cubeSide);
 
     const Vector3 front = up.cross(right);
-    const unsigned patchResolution = planetoid->patchResolution;
+    const unsigned patchResolution = planet->patchResolution;
     const double patchHalfSize = patch->halfSize;
     const double faceSize = (patchHalfSize * 2) / patchResolution;
-    const double planetoidMeanRadius = planetoid->meanRadius;
+    const double planetMeanRadius = planet->meanRadius;
 
     Mesh::Handle mesh(new Mesh());
     mesh->setPrimitiveType(PrimitiveType::Lines);
@@ -121,12 +179,12 @@ Mesh::Handle PlanetSystem::buildPatchMesh(Planet::Iterator planetoid, PlanetPatc
         position += front * y * faceSize;
         for (unsigned x = 0; x < patchResolution + 1; ++x)
         {
-            const Vector3 morphedPosition = morphPointToSphere(position, relativePosition, planetoidMeanRadius);
-            const Vector3 morphedNormal = (morphedPosition + relativePosition).normalized();
+            //const Vector3 morphedPosition = morphPointToSphere(position, relativePosition, planetMeanRadius);
+            //const Vector3 morphedNormal = (morphedPosition + relativePosition).normalized();
 
             meshWriter.addVertex();
-            meshWriter.writeAttributeData(VertexAttributeSemantic::Position, morphedPosition);
-            meshWriter.writeAttributeData(VertexAttributeSemantic::Normal, morphedNormal);
+            meshWriter.writeAttributeData(VertexAttributeSemantic::Position, position);
+            meshWriter.writeAttributeData(VertexAttributeSemantic::Normal, (position + relativePosition).normalized());
 
             position += right * faceSize;
         }
--- Source/Systems/PlanetSystem.h
+++ Source/Systems/PlanetSystem.h
@@ -24,12 +24,15 @@ public:
 
     void initialize() override;
     void tick(double timeStep) override;
-    void onComponentAdded(Planet::Iterator planetoid) override;
+    void onComponentAdded(Planet::Iterator planet) override;
 
 private:
+    void split(PlanetPatch::Iterator patch);
+
     Entity::Iterator createPatch(Entity::Iterator parent, CubeSide cubeSide, const Vector3& relativePosition);
-    Entity::Iterator createRootPatch(Planet::Iterator planetoid, CubeSide cubeSide);
-    Mesh::Handle buildPatchMesh(Planet::Iterator planetoid, PlanetPatch::Iterator patch, const Vector3& relativePosition);
+    Entity::Iterator createRootPatch(Planet::Iterator planet, CubeSide cubeSide);
+
+    Mesh::Handle buildPatchMesh(Planet::Iterator planet, PlanetPatch::Iterator patch, const Vector3& relativePosition);
 
     static Vector3 morphPointToSphere(const Vector3& point, const Vector3& relativePosition, double radius);
     static Vector3 projectUnitCubeToSphere(const Vector3& point);
@@ -37,9 +40,13 @@ private:
     static const Vector3& cubeSideUpVector(CubeSide cubeSide);
     static const Vector3& cubeSideRightVector(CubeSide cubeSide);
 
+    AssetCache& _assetCache;
+
     BoundingBoxSystem::Handle _boundingBoxSystem;
     CameraSystem::Handle _cameraSystem;
     TransformSystem::Handle _transformSystem;
+
+    Planet::Iterator _planet;
 };
 
 }
